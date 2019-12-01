60cda09053c208bcbc4c4439adea740395093754
shadysaad <45115969+shadysaad@users.noreply.github.com>
2019-12-01 13:15:28 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -20,8 +20,8 @@ const gif = require("gif-search");
 
 const client = new Discord.Client({disableEveryone: true});
 
-const prefix = "1";
-var adminprefix = '1'
+const prefix = "$";
+var adminprefix = '$'
 /////////////////////////
 ////////////////////////
 
